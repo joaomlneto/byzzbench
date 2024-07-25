98247107766fd02696a7a955d3d6b504f59c00e3
Restyled.io <commits@restyled.io>
2024-07-25 09:36:01 +0000

    Restyled by clang-format

--- simulator/src/main/java/byzzbench/simulator/protocols/XRPL/XRPLScenarioExecutor.java
+++ simulator/src/main/java/byzzbench/simulator/protocols/XRPL/XRPLScenarioExecutor.java
@@ -1,72 +1,67 @@
 package byzzbench.simulator.protocols.XRPL;
 
+import byzzbench.simulator.ScenarioExecutor;
+import byzzbench.simulator.protocols.XRPL.messages.XRPLTxMessage;
+import byzzbench.simulator.transport.Transport;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
-
-import byzzbench.simulator.ScenarioExecutor;
-import byzzbench.simulator.protocols.XRPL.messages.XRPLTxMessage;
-import byzzbench.simulator.transport.Transport;
 import lombok.extern.java.Log;
 
 @Log
-public class XRPLScenarioExecutor extends ScenarioExecutor<XRPLLedger>  {
-    private final int NUM_NODES = 3;
-    
-    
-    private List<XRPLReplica> replica_list;
+public class XRPLScenarioExecutor extends ScenarioExecutor<XRPLLedger> {
+  private final int NUM_NODES = 3;
 
-    public XRPLScenarioExecutor() {
-        super(new Transport<>());
+  private List<XRPLReplica> replica_list;
+
+  public XRPLScenarioExecutor() { super(new Transport<>()); }
+
+  @Override
+  public void setup() {
+    try {
+      Set<String> nodeIds = new TreeSet<>();
+      for (int i = 0; i < NUM_NODES; i++) {
+        nodeIds.add(Character.toString((char)('A' + i)));
+      }
+      this.replica_list = new ArrayList<>();
+      XRPLLedger genesis = new XRPLLedger("0", 1, new ArrayList<>());
+      nodeIds.forEach(nodeId -> {
+        // XRPLMessageLog messageLog = new XRPLMessageLog();
+        XRPLReplica replica =
+            new XRPLReplica(nodeId, nodeIds, this.transport, nodeIds,
+                            genesis); // nodes trust all nodes currently
+        this.replica_list.add(replica);
+        nodes.put(nodeId, replica);
+        transport.addNode(replica);
+      });
+      transport.registerMessageMutators(new XRPLProposeMessageMutatorFactory());
+      log.info("nodes: " + nodes);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    @Override
-    public void setup() {
-        try {
-            Set<String> nodeIds = new TreeSet<>();
-            for (int i = 0; i < NUM_NODES; i++) {
-                nodeIds.add(Character.toString((char) ('A' + i)));
-            }
-            this.replica_list = new ArrayList<>();
-            XRPLLedger genesis = new XRPLLedger( "0", 1, new ArrayList<>());
-            nodeIds.forEach(nodeId -> {
-                //XRPLMessageLog messageLog = new XRPLMessageLog();
-                XRPLReplica replica = new XRPLReplica(nodeId, nodeIds, this.transport, nodeIds, genesis); //nodes trust all nodes currently
-                this.replica_list.add(replica);
-                nodes.put(nodeId, replica);
-                transport.addNode(replica);
-            });
-            transport.registerMessageMutators(new XRPLProposeMessageMutatorFactory());
-            log.info("nodes: " + nodes);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+  @Override
+  public void run() {
+    try {
+      String tx1 = "0000";
+      String tx2 = "0001";
+
+      XRPLTxMessage txmsg1 = new XRPLTxMessage(tx1);
+      XRPLTxMessage txmsg2 = new XRPLTxMessage(tx2);
+
+      nodes.get("A").handleMessage("c", txmsg1);
+      nodes.get("B").handleMessage("c1", txmsg2);
+
+      // The first heartbeat to initialize
+      for (XRPLReplica xrplReplica : replica_list) {
+        xrplReplica.onHeartbeat();
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
     }
-
-    @Override
-    public void run() {
-        try {
-            String tx1 = "0000";
-            String tx2 = "0001";
-
-            XRPLTxMessage txmsg1 = new XRPLTxMessage(tx1);
-            XRPLTxMessage txmsg2 = new XRPLTxMessage(tx2);
-
-            nodes.get("A").handleMessage("c", txmsg1);
-            nodes.get("B").handleMessage("c1", txmsg2);
-
-            //The first heartbeat to initialize
-            for (XRPLReplica xrplReplica : replica_list) {
-                xrplReplica.onHeartbeat();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-        
-        
-    }
-
+  }
 }
--- simulator/src/main/java/byzzbench/simulator/protocols/XRPL/messages/XRPLProposeMessage.java
+++ simulator/src/main/java/byzzbench/simulator/protocols/XRPL/messages/XRPLProposeMessage.java
@@ -6,25 +6,20 @@ import lombok.With;
 
 @With
 public class XRPLProposeMessage implements MessagePayload {
-    private XRPLProposal prop;
-    private String senderId;
+  private XRPLProposal prop;
+  private String senderId;
 
-    public XRPLProposeMessage(XRPLProposal prop_, String id) {
-        this.prop = prop_;
-        this.senderId = id;
-    }
+  public XRPLProposeMessage(XRPLProposal prop_, String id) {
+    this.prop = prop_;
+    this.senderId = id;
+  }
 
-    @Override
-    public String getType() {
-        return "PROPOSE";
-    }
+  @Override
+  public String getType() {
+    return "PROPOSE";
+  }
 
-    public String getSenderId() {
-        return this.senderId;
-    }    
+  public String getSenderId() { return this.senderId; }
 
-    public XRPLProposal getProposal() {
-        return this.prop;
-    }
-
+  public XRPLProposal getProposal() { return this.prop; }
 }
