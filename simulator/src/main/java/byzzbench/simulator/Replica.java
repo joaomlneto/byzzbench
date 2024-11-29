ec32bf066b03e73441d88c5b4db8e4060754509b
Attila Birke <A.B.Birke@student.tudelft.nl>
2024-11-29 14:24:50 +0100

    Changed commitLog to a Map<Long, LogEntry> where the results are logged to a sequence number.

--- simulator/src/main/java/byzzbench/simulator/Replica.java
+++ simulator/src/main/java/byzzbench/simulator/Replica.java
@@ -173,8 +173,8 @@ public abstract class Replica implements Serializable, Node {
      *
      * @param operation the operation to commit
      */
-    public void commitOperation(LogEntry operation) {
-        this.commitLog.add(operation);
+    public void commitOperation(long sequenceNumber, LogEntry operation) {
+        this.commitLog.add(sequenceNumber, operation);
         this.notifyObserversLocalCommit(operation);
     }
 
