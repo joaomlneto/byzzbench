0eb19c8bd481330f5ef6e7a49b2f5feab024e82d
Attila Birke <A.B.Birke@student.tudelft.nl>
2024-11-29 13:40:23 +0100

    more tests

--- simulator/src/test/java/byzzbench/simulator/protocols/hbft/HbftJavaReplicaTests.java
+++ simulator/src/test/java/byzzbench/simulator/protocols/hbft/HbftJavaReplicaTests.java
@@ -9,8 +9,11 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import byzzbench.simulator.protocols.hbft.message.CommitMessage;
 import byzzbench.simulator.protocols.hbft.message.PrepareMessage;
 import byzzbench.simulator.protocols.hbft.message.RequestMessage;
+import byzzbench.simulator.protocols.hbft.pojo.ReplicaRequestKey;
+import byzzbench.simulator.protocols.hbft.pojo.ReplicaTicketPhase;
 import byzzbench.simulator.scheduler.Scheduler;
 
 @SpringBootTest
@@ -58,32 +61,119 @@ public class HbftJavaReplicaTests {
     }
 
     @Test
-	void testRecvWrongPrepare() {
+	void testRecvWrongSeqPrepares() {
         long viewNumber = 1;
         long seqNumber = 2;
         RequestMessage request = new RequestMessage("123", 0, "C0");
         byte[] digest = replica.digest(request);
         PrepareMessage prepare = new PrepareMessage(viewNumber, seqNumber, digest, request);
+        PrepareMessage prepare0 = new PrepareMessage(viewNumber, 0, digest, request);
+        
+        replica.recvPrepare(prepare);
+        replica.recvPrepare(prepare0);
+
+        // Prepare message should not be accepted so ticket should be null
+        Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, seqNumber) == null, "Prepare messages with incorrect seqNum not accepted!");
+        Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, 0) == null, "Prepare messages with incorrect seqNum not accepted!");
+    }
+
+    @Test
+	void testRecvWrongViewPrepares() {
+        long viewNumber = 2;
+        long seqNumber = 1;
+        RequestMessage request = new RequestMessage("123", 0, "C0");
+        byte[] digest = replica.digest(request);
+        PrepareMessage prepare = new PrepareMessage(viewNumber, seqNumber, digest, request);
+        PrepareMessage prepare0 = new PrepareMessage(0, seqNumber, digest, request);
+        
+        replica.recvPrepare(prepare);
+        replica.recvPrepare(prepare0);
+
+        // Prepare message should not be accepted so ticket should be null
+        Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, seqNumber) == null, "Prepare messages with incorrect viewNum not accepted!");
+        Assert.isTrue(replica.getMessageLog().getTicket(0, seqNumber) == null, "Prepare messages with incorrect viewNum not accepted!");
+    }
+    
+    @Test
+	void testRecvWrongDigestPrepare() {
+        long viewNumber = 1;
+        long seqNumber = 1;
+        RequestMessage request = new RequestMessage("123", 0, "C0");
+        RequestMessage request2 = new RequestMessage("321", 100, "C0");
+        byte[] digest = replica.digest(request2);
+        PrepareMessage prepare = new PrepareMessage(viewNumber, seqNumber, digest, request);
         
         replica.recvPrepare(prepare);
 
-        // Prepare message should not be accepted so ticket should be null
-        Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, seqNumber) == null, "Prepare message with incorrect seqNum not accepted!");
+        // Prepare message should not be accepted
+        Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, seqNumber).getMessages() == null, "Prepare with incorrect digest should not be accepted!");
     }
-    
-    // @Test
-	// void testRecvCommit() {
-    //     long viewNumber = 1;
-    //     long seqNumber = 1;
-    //     RequestMessage request = new RequestMessage("123", 0, "C0");
-    //     byte[] digest = replica.digest(request);
-    //     PrepareMessage prepare = new PrepareMessage(viewNumber, seqNumber, digest, request);
-    //     CommitMessage commit = new CommitMessage(viewNumber, seqNumber, digest, request, "B", this.replica.getSpeculativeHistory());
 
-    //     replica.recvPrepare(prepare);
+    @Test
+	void testRecvDiffSeqWithAlreadyAcceptedPrepare() {
+        long viewNumber = 1;
+        long seqNumber = 1;
+        RequestMessage request = new RequestMessage("123", 0, "C0");
+        byte[] digest = replica.digest(request);
+        PrepareMessage prepare = new PrepareMessage(viewNumber, seqNumber, digest, request);
+        PrepareMessage prepare2 = new PrepareMessage(viewNumber, seqNumber + 1, digest, request);
+        
+        replica.recvPrepare(prepare);
 
-    //     // Prepare message should be accepted
-    //     Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, seqNumber).getMessages().contains(commit), "Commit message with correct seqNum and viewNum should be accepted!");
-    //     Assert.isTrue(!replica.getMessageLog().getTicket(viewNumber, seqNumber).isCommittedLocal(tolerance), "Commit message with correct seqNum and viewNum should be accepted!");
-    // }
+        // Correct prepare should be accepted
+        Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, seqNumber).getMessages().contains(prepare), "Accept first prepare!");
+
+        replica.recvPrepare(prepare2);
+
+        // Prepare message should not be accepted
+        Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, seqNumber + 1).getMessages() == null, "Correct prepare with already ");
+    }
+
+    @Test
+	void testRecvCommit() {
+        long viewNumber = 1;
+        long seqNumber = 1;
+        RequestMessage request = new RequestMessage("123", 0, "C0");
+        byte[] digest = replica.digest(request);
+        //PrepareMessage prepare = new PrepareMessage(viewNumber, seqNumber, digest, request);
+        CommitMessage commit = new CommitMessage(viewNumber, seqNumber, digest, request, "B", this.replica.getSpeculativeHistory());
+
+        replica.recvCommit(commit);
+
+        // Commit message should be accepted
+        Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, seqNumber).getMessages().contains(commit), "Commit message with correct seqNum and viewNum should be accepted!");
+        Assert.isTrue(!replica.getMessageLog().getTicket(viewNumber, seqNumber).isCommittedLocal(tolerance), "This commit should not accept local commit!");
+        Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, seqNumber).getPhase() == ReplicaTicketPhase.PREPARE, "Replica should still be in prepare phase!");
+
+        CommitMessage commit2 = new CommitMessage(viewNumber, seqNumber, digest, request, "C", this.replica.getSpeculativeHistory());
+        replica.recvCommit(commit2);
+
+        ReplicaRequestKey key = new ReplicaRequestKey("C0", 0);
+        Assert.isTrue(replica.getMessageLog().getTicketFromCache(key).isPrepared(tolerance), "Should have received f + 1 commits!");
+        Assert.isTrue(replica.getMessageLog().getTicketFromCache(key).isCommittedLocal(tolerance), "Should send commit and thus have 2f + 1 commits!");
+    }
+
+    @Test
+	void testRecvCommitAfterPrepare() {
+        long viewNumber = 1;
+        long seqNumber = 1;
+        RequestMessage request = new RequestMessage("123", 0, "C0");
+        byte[] digest = replica.digest(request);
+        PrepareMessage prepare = new PrepareMessage(viewNumber, seqNumber, digest, request);
+        CommitMessage commit = new CommitMessage(viewNumber, seqNumber, digest, request, "B", this.replica.getSpeculativeHistory());
+
+        replica.recvPrepare(prepare);
+        replica.recvCommit(commit);
+
+        // Commit message should be accepted
+        Assert.isTrue(!replica.getMessageLog().getTicket(viewNumber, seqNumber).isCommittedLocal(tolerance), "This commit should not accept local commit!");
+        Assert.isTrue(replica.getMessageLog().getTicket(viewNumber, seqNumber).getPhase() == ReplicaTicketPhase.COMMIT, "Replica should be in commit phase!");
+
+        CommitMessage commit2 = new CommitMessage(viewNumber, seqNumber, digest, request, "B", this.replica.getSpeculativeHistory());
+        replica.recvCommit(commit2);
+
+        ReplicaRequestKey key = new ReplicaRequestKey("C0", 0);
+        Assert.isTrue(replica.getMessageLog().getTicketFromCache(key).isPrepared(tolerance), "Should have received f + 1 commits!");
+        Assert.isTrue(replica.getMessageLog().getTicketFromCache(key).isCommittedLocal(tolerance), "Should send commit and thus have 2f + 1 commits!");
+    }
 }
