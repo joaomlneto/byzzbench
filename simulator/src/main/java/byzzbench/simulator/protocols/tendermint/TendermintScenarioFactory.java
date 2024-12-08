0f558b091fd417a77e6c0f858c516eb5b7f2ff0b
Antoni Nowakowski <antoni.nowakowski@robinradar.com>
2024-12-08 11:35:23 +0100

    WIP

--- simulator/src/main/java/byzzbench/simulator/protocols/tendermint/TendermintScenarioFactory.java
+++ simulator/src/main/java/byzzbench/simulator/protocols/tendermint/TendermintScenarioFactory.java
@@ -2,16 +2,18 @@ package byzzbench.simulator.protocols.tendermint;
 
 import byzzbench.simulator.BaseScenarioFactory;
 import byzzbench.simulator.Scenario;
+import byzzbench.simulator.config.ByzzBenchConfig;
 import byzzbench.simulator.scheduler.Scheduler;
 import byzzbench.simulator.service.MessageMutatorService;
 import byzzbench.simulator.service.SchedulerFactoryService;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TendermintScenarioFactory extends BaseScenarioFactory {
-    public TendermintScenarioFactory(SchedulerFactoryService schedulerFactoryService) {
-        super(schedulerFactoryService);
+    public TendermintScenarioFactory(SchedulerFactoryService schedulerFactoryService, ByzzBenchConfig byzzBenchConfig, ObjectMapper objectMapper) {
+        super(schedulerFactoryService, byzzBenchConfig, objectMapper);
     }
 
     @Override
