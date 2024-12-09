7c5873c5af9f1241459552f0211683ea5810470d
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-12-09 15:59:01 +0100

    docs: custom completable future

--- simulator/src/main/java/byzzbench/simulator/utils/FIFOCompletableFuture.java
+++ simulator/src/main/java/byzzbench/simulator/utils/FIFOCompletableFuture.java
@@ -4,7 +4,13 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-public class DeterministicCompletableFuture<T> extends CompletableFuture<T> {
+/**
+ * A {@link CompletableFuture} that uses a single thread {@link Executor} to guarantee ordering of dependent tasks.
+ * Dependent tasks are executed in the order they are added (FIFO).
+ *
+ * @param <T> The result type returned by this future's {@code join} and {@code get} methods
+ */
+public class FIFOCompletableFuture<T> extends CompletableFuture<T> {
     private static final Executor executor = Executors.newSingleThreadExecutor(
             runnable -> new Thread(runnable, "Custom-Single-Thread")
     );
