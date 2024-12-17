1275a89dadb5f1bbde4c5d355482c319c59f4862
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-12-17 10:51:03 +0100

    compute possible ways to split n elements into k non-empty subsets

--- simulator/src/main/java/byzzbench/simulator/utils/StirlingNumberSecondKind.java
+++ simulator/src/main/java/byzzbench/simulator/utils/StirlingNumberSecondKind.java
@@ -50,12 +50,74 @@ public class StirlingNumberSecondKind {
      * Get the partition associated with the nth Stirling number of the second kind.
      *
      * @param elements the elements to partition
-     * @param n        the index
+     * @param k        the number of non-empty subsets
+     * @param i        the i-th partition to get
      * @param <T>      the type of the elements
      * @return k partitions of the elements
      */
-    public static <T> Collection<Collection<T>> getNthPartition(Collection<T> elements, int n) {
-        throw new UnsupportedOperationException("Not implemented");
+    public static <T> List<List<T>> getIthPartition(Collection<T> elements, int k, int i) {
+        List<List<List<T>>> partitions = getPartitions(new ArrayList<>(elements), k);
+        return partitions.get(i % partitions.size());
+    }
+
+    /**
+     * Generate all partitions of the elements into k non-empty subsets.
+     *
+     * @param elements the elements to partition
+     * @param k        the number of non-empty subsets
+     * @param <T>      the type of the elements
+     * @return k partitions of the elements
+     */
+    public static <T> List<List<List<T>>> getPartitions(List<T> elements, int k) {
+        // initialize result
+        List<List<List<T>>> result = new ArrayList<>();
+
+        // initialize k partitions
+        List<List<T>> partitions = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            partitions.add(new ArrayList<>());
+        }
+
+        // compute partitions
+        getPartitions(result, partitions, elements, k, k, elements.size(), 0, -1);
+
+        return result;
+    }
+
+    /**
+     * Recursive helper function to compute partitions.
+     */
+    private static <T> void getPartitions(
+            List<List<List<T>>> result,
+            List<List<T>> parts,
+            List<T> elements,
+            int k,
+            int empty,
+            int n,
+            int m,
+            int lastfilled) {
+        if (m == n) {
+            //System.out.println("m == n: " + parts);
+            List<List<T>> partition = new ArrayList<>();
+            for (List<T> part : parts) {
+                partition.add(new ArrayList<>(part));
+            }
+            result.add(partition);
+            return;
+        }
+        int start = (n - m == empty) ? (k - empty) : 0;
+
+        for (int i = start; i < Math.min(k, lastfilled + 2); i++) {
+            parts.get(i).add(elements.get(m));
+            if (parts.get(i).size() == 1) {
+                empty -= 1;
+            }
+            getPartitions(result, parts, elements, k, empty, n, m + 1, Math.max(i, lastfilled));
+            parts.get(i).removeLast();
+            if (parts.get(i).isEmpty()) {
+                empty += 1;
+            }
+        }
     }
 
     /**
