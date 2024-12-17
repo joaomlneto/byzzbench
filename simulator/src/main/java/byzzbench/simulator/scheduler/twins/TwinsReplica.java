f965514ebc459296979bc78a9ab629c12da4479c
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-12-17 17:54:33 +0100

    Twins: message handling complete-ish
    still missing timeouts

--- simulator/src/main/java/byzzbench/simulator/scheduler/twins/TwinsReplica.java
+++ simulator/src/main/java/byzzbench/simulator/scheduler/twins/TwinsReplica.java
@@ -3,12 +3,15 @@ package byzzbench.simulator.scheduler.twins;
 import byzzbench.simulator.Replica;
 import byzzbench.simulator.state.TotalOrderCommitLog;
 import byzzbench.simulator.transport.MessagePayload;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import byzzbench.simulator.transport.messages.MessageWithRound;
+import byzzbench.simulator.utils.StirlingNumberSecondKind;
 import lombok.Getter;
 import lombok.extern.java.Log;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * A Twins {@link Replica} that emulates byzantine behavior
@@ -18,14 +21,14 @@ import java.util.ArrayList;
  * Andrey Chursin, Dmitri Perelman, Zekun Li, Avery Ching and Dahlia Malkhi.
  * https://drops.dagstuhl.de/entities/document/10.4230/LIPIcs.OPODIS.2021.7
  */
+@Getter
 @Log
 public class TwinsReplica extends Replica {
-    @JsonIgnore
     private final TwinsTransport twinsTransport;
+
     /**
      * The list of replicas that are part of this Twins replica.
      */
-    @Getter
     private final ArrayList<Replica> replicas = new ArrayList<>();
 
     /**
@@ -36,8 +39,6 @@ public class TwinsReplica extends Replica {
      */
     public TwinsReplica(Replica replica, int numTwins) {
         super(replica.getId(), replica.getScenario(), new TotalOrderCommitLog());
-
-        this.twinsTransport = new TwinsTransport(replica.getScenario());
 
         if (numTwins < 2) {
             throw new IllegalArgumentException("numTwins must be at least 2");
@@ -50,15 +51,117 @@ public class TwinsReplica extends Replica {
             replicas.add(twin);
             twin.initialize();
         }
+
+        // Update the replicas to use a Twins transport
+        // Note: this must be done after the replicas are created and added to the list!
+        this.twinsTransport = new TwinsTransport(this);
+        this.replicas.forEach(r -> r.setTransport(this.twinsTransport));
+
+        // Create some round partitions. TODO: parameterize!
+        List<String> nodeIds = new ArrayList<>(replica.getNodeIds().stream().filter(id -> !id.equals(this.getId())).toList());
+        nodeIds.addAll(getInternalIds());
+        List<List<List<String>>> options = StirlingNumberSecondKind.getPartitions(nodeIds, 2);
+        // select a random option
+        Random rand = new Random();
+        for (long i = 0; i < 5; i++) {
+            this.twinsTransport.getRoundPartitions().put(i, options.get(rand.nextInt(options.size())));
+        }
+
+        // print getRoundPartitions, one per line
+        this.twinsTransport.getRoundPartitions().forEach((k, v) -> log.info("Round " + k + ": " + v));
+    }
+
+    /**
+     * Get the internal "virtual" replica IDs.
+     *
+     * @return the internal IDs
+     */
+    protected List<String> getInternalIds() {
+        List<String> ids = new ArrayList<>();
+        for (int i = 0; i < replicas.size(); i++) {
+            ids.add(String.format("%s:%d", getId(), i));
+        }
+        return ids;
+    }
+
+    /**
+     * Get the internal ID for the given replica.
+     *
+     * @param replica the replica to get the ID for
+     * @return the internal ID for the internal "twin" replica
+     */
+    protected String getInternalId(Replica replica) {
+        return String.format("%s:%d", getId(), replicas.indexOf(replica));
+    }
+
+    /**
+     * Check if the given ID is an internal ID.
+     *
+     * @param id the ID to check
+     * @return true if the ID is an internal ID
+     */
+    private boolean isInternalId(String id) {
+        return id.startsWith(getId() + ":");
+    }
+
+    /**
+     * Get the Replica object corresponding to the given ID.
+     *
+     * @param id the ID of the replica
+     * @return the replica object
+     */
+    private Replica getInternalReplicaFromId(String id) {
+        if (!isInternalId(id)) {
+            throw new IllegalArgumentException("Not an internal ID: " + id);
+        }
+        int index = Integer.parseInt(id.substring(getId().length() + 1));
+        return replicas.get(index);
     }
 
     @Override
-    public void handleClientRequest(String clientId, Serializable request) throws Exception {
-        throw new UnsupportedOperationException("Not yet implemented!");
+    public void handleClientRequest(String clientId, Serializable request) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    /**
+     * Get the internal replicas that should handle the given message.
+     *
+     * @param sender  the sender of the message
+     * @param message the message to deliver
+     * @return the list of internal replicas that should handle the message
+     */
+    public List<Replica> getInternalReplicasHandlingMessage(String sender, MessagePayload message) {
+        List<String> partition;
+        if (message instanceof MessageWithRound messageWithRound) {
+            // If the message has a round number, use the partition for that round
+            partition = this.twinsTransport.getReplicaRoundPartition(sender, messageWithRound.getRound());
+        } else {
+            // Default to the default partition
+            // If the sender is not in *any* partition, default to delivering to all internal replicas
+            partition = this.twinsTransport.getDefaultPartitions()
+                    .stream()
+                    .filter(p -> p.contains(sender))
+                    .findFirst()
+                    .orElse(this.getInternalIds()); // default to all internal replicas handling the message
+        }
+
+        if (partition == null) {
+            throw new IllegalArgumentException("Sender not found in any partition: " + sender);
+        }
+
+        return partition.stream()
+                .filter(this::isInternalId)
+                .map(this::getInternalReplicaFromId)
+                .toList();
     }
 
     @Override
     public void handleMessage(String sender, MessagePayload message) throws Exception {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        List<Replica> internalReplicas = this.getInternalReplicasHandlingMessage(sender, message);
+
+        // deliver to all sub-replicas
+        for (Replica internalReplica : internalReplicas) {
+            internalReplica.handleMessage(sender, message);
+        }
     }
 }
--- simulator/src/main/java/byzzbench/simulator/scheduler/twins/TwinsTransport.java
+++ simulator/src/main/java/byzzbench/simulator/scheduler/twins/TwinsTransport.java
@@ -1,14 +1,187 @@
 package byzzbench.simulator.scheduler.twins;
 
-import byzzbench.simulator.Scenario;
+import byzzbench.simulator.Node;
+import byzzbench.simulator.Replica;
+import byzzbench.simulator.transport.MessagePayload;
 import byzzbench.simulator.transport.Transport;
+import byzzbench.simulator.transport.messages.MessageWithRound;
+import lombok.Getter;
+import lombok.extern.java.Log;
+
+import java.io.Serializable;
+import java.time.Duration;
+import java.util.*;
 
 /**
  * A transport layer that is used by the {@link TwinsReplica} to route messages
  * internally between the twin replica instances.
  */
-public class TwinsTransport extends Transport {
-    public TwinsTransport(Scenario scenario) {
-        super(scenario);
+@Log
+public class TwinsTransport extends Transport implements Serializable {
+    /**
+     * Partitions to use for the replicas if another partition is not specified.
+     */
+    @Getter
+    private final List<List<String>> defaultPartitions;
+
+    /**
+     * Scheduled partitions to use for each round.
+     */
+    @Getter
+    private final Map<Long, List<List<String>>> roundPartitions = new TreeMap<>();
+
+    /**
+     * The Twins Replica using this transport.
+     */
+    private final TwinsReplica twinsReplica;
+
+    /**
+     * Create a new Twins transport layer for the given Twins replica.
+     *
+     * @param twinsReplica the Twins replica
+     */
+    public TwinsTransport(TwinsReplica twinsReplica) {
+        super(twinsReplica.getScenario());
+        this.twinsReplica = twinsReplica;
+
+        // By default, both internal replicas will be connected to the network.
+        List<String> singlePartition = new ArrayList<>();
+        singlePartition.addAll(twinsReplica.getNodeIds().stream().filter(id -> !id.equals(twinsReplica.getId())).toList());
+        singlePartition.addAll(twinsReplica.getInternalIds());
+        this.defaultPartitions = List.of(singlePartition);
+    }
+
+
+    /**
+     * Get the partitions to use for a given round number
+     *
+     * @param roundNumber the round number
+     * @return the partitions to use
+     */
+    public List<List<String>> getRoundPartition(long roundNumber) {
+        return this.roundPartitions.getOrDefault(roundNumber, this.defaultPartitions);
+    }
+
+    /**
+     * Get the partition that the given replica ID is in for the given round number.
+     *
+     * @param replicaId   the replica ID
+     * @param roundNumber the round number
+     * @return the partition
+     */
+    public List<String> getReplicaRoundPartition(String replicaId, long roundNumber) {
+        List<List<String>> partition = getRoundPartition(roundNumber);
+
+        for (List<String> replicaPartition : partition) {
+            if (replicaPartition.contains(replicaId)) {
+                return replicaPartition;
+            }
+        }
+
+        throw new IllegalArgumentException("Replica ID not found in any partition: " + replicaId);
+    }
+
+    /**
+     * Check if two nodes can communicate with each other, given a specific partition.
+     *
+     * @param sender    the sender node (internal ID of internal "twin" replica)
+     * @param recipient the recipient node
+     * @return true if the two nodes can communicate
+     */
+    public boolean canSendMessage(String sender, String recipient, MessagePayload message) {
+        List<String> partition;
+        if (message instanceof MessageWithRound messageWithRound) {
+            // If the message has a round number, use the partition for that round
+            partition = getReplicaRoundPartition(sender, messageWithRound.getRound());
+        } else {
+            // Default to the default partition
+            partition = getDefaultPartitions()
+                    .stream()
+                    .filter(p -> p.contains(sender))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Sender not found in any partition: " + sender));
+        }
+
+        if (partition == null) {
+            throw new IllegalArgumentException("Sender not found in any partition: " + sender);
+        }
+
+        return partition.contains(recipient);
+    }
+
+    @Override
+    public synchronized void sendMessage(Node sender, MessagePayload message, String recipient) {
+        // Sanity check
+        if (!(sender instanceof Replica replicaSender)) {
+            throw new IllegalArgumentException("Sender must be a Replica");
+        }
+
+        String internalId = this.twinsReplica.getInternalId(replicaSender);
+
+        if (canSendMessage(internalId, recipient, message)) {
+            this.getScenario().getTransport().sendMessage(sender, message, recipient);
+        } else {
+            log.info("Message from " + internalId + " to " + recipient + " blocked by partitioning");
+        }
+    }
+
+    @Override
+    public synchronized void multicast(Node sender, SortedSet<String> recipients, MessagePayload payload) {
+        // Sanity check
+        if (!(sender instanceof Replica replicaSender)) {
+            throw new IllegalArgumentException("Sender must be a Replica");
+        }
+
+        String internalId = this.twinsReplica.getInternalId(replicaSender);
+
+        // Send the message to the available recipients, if any
+        List<String> availableRecipients = recipients.stream()
+                .filter(recipient -> canSendMessage(internalId, recipient, payload))
+                .toList();
+        if (!availableRecipients.isEmpty()) {
+            this.getScenario().getTransport().multicast(sender, new TreeSet<>(availableRecipients), payload);
+        }
+
+        // Log the dropped recipients, if any
+        List<String> droppedRecipients = recipients.stream()
+                .filter(recipient -> !canSendMessage(internalId, recipient, payload))
+                .toList();
+        if (!droppedRecipients.isEmpty()) {
+            log.info("Message from " + internalId + " to " + droppedRecipients + " blocked by partitioning");
+        }
+    }
+
+    @Override
+    public synchronized void sendClientResponse(Node sender, MessagePayload response, String recipient) {
+        // Sanity check
+        if (!(sender instanceof Replica replicaSender)) {
+            throw new IllegalArgumentException("Sender must be a Replica");
+        }
+
+        String internalId = this.twinsReplica.getInternalId(replicaSender);
+
+        if (canSendMessage(internalId, recipient, response)) {
+            this.getScenario().getTransport().sendMessage(sender, response, recipient);
+        } else {
+            log.info("Message from " + internalId + " to " + recipient + " blocked by partitioning");
+        }
+    }
+
+    @Override
+    public synchronized long setTimeout(Node node, Runnable runnable, Duration timeout) {
+        //return super.setTimeout(node, runnable, timeout);
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public synchronized void clearTimeout(Node node, long eventId) {
+        //super.clearTimeout(node, eventId);
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public synchronized void clearReplicaTimeouts(Node node) {
+        //super.clearReplicaTimeouts(node);
+        throw new UnsupportedOperationException("Not implemented");
     }
 }
