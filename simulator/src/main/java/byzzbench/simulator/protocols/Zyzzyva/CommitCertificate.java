1af3bf7b325811f9b9d366dab09d0ceb72143754
Tezzish <ishanpahwa14@gmail.com>
2025-01-07 19:06:10 +0100

    Changes structure for Commit Certificate

--- simulator/src/main/java/byzzbench/simulator/protocols/Zyzzyva/CommitCertificate.java
+++ simulator/src/main/java/byzzbench/simulator/protocols/Zyzzyva/CommitCertificate.java
@@ -4,19 +4,22 @@ import byzzbench.simulator.protocols.Zyzzyva.message.SpeculativeResponse;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.SortedMap;
 
 @Data
 public class CommitCertificate implements Serializable {
     final long sequenceNumber;
     final long viewNumber;
-    List<SpeculativeResponse> speculativeResponses;
-    List<String> replicaIds;
+    final long timestamp;
+    final byte[] digest;
+    SortedMap<String, SpeculativeResponse> speculativeResponses;
     String clientId;
 
-    public CommitCertificate(long sequenceNumber, long viewNumber, List<SpeculativeResponse> speculativeResponses) {
+    public CommitCertificate(long sequenceNumber, long viewNumber, long timestamp, byte[] digest, SortedMap<String, SpeculativeResponse> speculativeResponses) {
         this.sequenceNumber = sequenceNumber;
         this.viewNumber = viewNumber;
+        this.timestamp = timestamp;
+        this.digest = digest;
         this.speculativeResponses = speculativeResponses;
     }
 }
