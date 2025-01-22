8c582cd1101030f314439c212c8ea6a815a1ee4c
abirke <a.b.birke@student.tudelft.nl>
2025-01-22 15:39:51 +0100

    Important round mutation change

--- simulator/src/main/java/byzzbench/simulator/protocols/hbft/message/IPhaseMessage.java
+++ simulator/src/main/java/byzzbench/simulator/protocols/hbft/message/IPhaseMessage.java
@@ -1,12 +1,22 @@
 package byzzbench.simulator.protocols.hbft.message;
 
 import byzzbench.simulator.transport.MessagePayload;
+import byzzbench.simulator.transport.messages.MessageWithRound;
 
-public abstract class IPhaseMessage extends MessagePayload {
+public abstract class IPhaseMessage extends MessagePayload implements MessageWithRound {
     public abstract long getViewNumber();
 
     public abstract long getSequenceNumber();
 
     public abstract byte[] getDigest();
+
+    /**
+     * Get the request of the message.
+     *
+     * @return The request of the message.
+     */
+    public long getRound() {
+        return getSequenceNumber();
+    }
     
 }
