4c7da2636b4291820b11d4af7a6daf56bdf7771a
abirke <a.b.birke@student.tudelft.nl>
2025-01-23 17:43:17 +0100

    Changed to any-scope

--- simulator/src/main/java/byzzbench/simulator/protocols/hbft/mutator/PrepareMessageMutatorFactory.java
+++ simulator/src/main/java/byzzbench/simulator/protocols/hbft/mutator/PrepareMessageMutatorFactory.java
@@ -24,102 +24,6 @@ public class PrepareMessageMutatorFactory extends MessageMutatorFactory {
     @Override
     public List<MessageMutationFault> mutators() {
         return List.of(
-                new MessageMutationFault("hbft-prepare-view-inc", "Increment View Number", List.of(PrepareMessage.class)) {
-                    @Override
-                    public void accept(FaultContext serializable) {
-                        Optional<Event> event = serializable.getEvent();
-                        if (event.isEmpty()) {
-                            throw invalidMessageTypeException;
-                        }
-                        if (!(event.get() instanceof MessageEvent messageEvent)) {
-                            throw invalidMessageTypeException;
-                        }
-                        if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
-                            throw invalidMessageTypeException;
-                        }
-                        PrepareMessage mutatedMessage = message.withViewNumber(message.getViewNumber() + 1);
-                        mutatedMessage.sign(message.getSignedBy());
-                        messageEvent.setPayload(mutatedMessage);
-                    }
-                },
-                new MessageMutationFault("hbft-prepare-view-dec", "Decrement View Number", List.of(PrepareMessage.class)) {
-                    @Override
-                    public void accept(FaultContext serializable) {
-                        Optional<Event> event = serializable.getEvent();
-                        if (event.isEmpty()) {
-                            throw invalidMessageTypeException;
-                        }
-                        if (!(event.get() instanceof MessageEvent messageEvent)) {
-                            throw invalidMessageTypeException;
-                        }
-                        if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
-                            throw invalidMessageTypeException;
-                        }
-                        PrepareMessage mutatedMessage = message.withViewNumber(message.getViewNumber() - 1);
-                        mutatedMessage.sign(message.getSignedBy());
-                        messageEvent.setPayload(mutatedMessage);
-                    }
-                },
-                new MessageMutationFault("hbft-prepare-seq-inc", "Increment Sequence Number", List.of(PrepareMessage.class)) {
-                    @Override
-                    public void accept(FaultContext serializable) {
-                        Optional<Event> event = serializable.getEvent();
-                        if (event.isEmpty()) {
-                            throw invalidMessageTypeException;
-                        }
-                        if (!(event.get() instanceof MessageEvent messageEvent)) {
-                            throw invalidMessageTypeException;
-                        }
-                        if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
-                            throw invalidMessageTypeException;
-                        }
-
-                        PrepareMessage mutatedMessage = message.withSequenceNumber(message.getSequenceNumber() + 1);
-                        mutatedMessage.sign(message.getSignedBy());
-                        messageEvent.setPayload(mutatedMessage);
-                    }
-                },
-                new MessageMutationFault("hbft-prepare-sec-dec", "Decrement Sequence Number", List.of(PrepareMessage.class)) {
-                    @Override
-                    public void accept(FaultContext serializable) {
-                        Optional<Event> event = serializable.getEvent();
-                        if (event.isEmpty()) {
-                            throw invalidMessageTypeException;
-                        }
-                        if (!(event.get() instanceof MessageEvent messageEvent)) {
-                            throw invalidMessageTypeException;
-                        }
-                        if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
-                            throw invalidMessageTypeException;
-                        }
-                        PrepareMessage mutatedMessage = message.withSequenceNumber(message.getSequenceNumber() - 1);
-                        mutatedMessage.sign(message.getSignedBy());
-                        messageEvent.setPayload(mutatedMessage);
-                    }
-                },
-                new MessageMutationFault("hbft-prepare-different-digest", "Change digest", List.of(PrepareMessage.class)) {
-                    @Override
-                    public void accept(FaultContext serializable) {
-                        Optional<Event> event = serializable.getEvent();
-                        if (event.isEmpty()) {
-                            throw invalidMessageTypeException;
-                        }
-                        if (!(event.get() instanceof MessageEvent messageEvent)) {
-                            throw invalidMessageTypeException;
-                        }
-                        if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
-                            throw invalidMessageTypeException;
-                        }
-                        // Create a random digest
-                        byte[] digest = new byte[20];
-                        PrepareMessage mutatedMessage = message.withDigest(digest);
-                        mutatedMessage.sign(message.getSignedBy());
-                        messageEvent.setPayload(mutatedMessage);
-                    }
-                }
-
-                // ANY-SCOPE mutations
-                //,
                 // new MessageMutationFault("hbft-prepare-view-inc", "Increment View Number", List.of(PrepareMessage.class)) {
                 //     @Override
                 //     public void accept(FaultContext serializable) {
@@ -133,7 +37,7 @@ public class PrepareMessageMutatorFactory extends MessageMutatorFactory {
                 //         if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
                 //             throw invalidMessageTypeException;
                 //         }
-                //         PrepareMessage mutatedMessage = message.withViewNumber(message.getViewNumber() + random.nextLong(bound));
+                //         PrepareMessage mutatedMessage = message.withViewNumber(message.getViewNumber() + 1);
                 //         mutatedMessage.sign(message.getSignedBy());
                 //         messageEvent.setPayload(mutatedMessage);
                 //     }
@@ -151,7 +55,7 @@ public class PrepareMessageMutatorFactory extends MessageMutatorFactory {
                 //         if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
                 //             throw invalidMessageTypeException;
                 //         }
-                //         PrepareMessage mutatedMessage = message.withViewNumber(message.getViewNumber() - random.nextLong(bound));
+                //         PrepareMessage mutatedMessage = message.withViewNumber(message.getViewNumber() - 1);
                 //         mutatedMessage.sign(message.getSignedBy());
                 //         messageEvent.setPayload(mutatedMessage);
                 //     }
@@ -170,7 +74,7 @@ public class PrepareMessageMutatorFactory extends MessageMutatorFactory {
                 //             throw invalidMessageTypeException;
                 //         }
 
-                //         PrepareMessage mutatedMessage = message.withSequenceNumber(message.getSequenceNumber() + random.nextLong(bound));
+                //         PrepareMessage mutatedMessage = message.withSequenceNumber(message.getSequenceNumber() + 1);
                 //         mutatedMessage.sign(message.getSignedBy());
                 //         messageEvent.setPayload(mutatedMessage);
                 //     }
@@ -188,11 +92,107 @@ public class PrepareMessageMutatorFactory extends MessageMutatorFactory {
                 //         if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
                 //             throw invalidMessageTypeException;
                 //         }
-                //         PrepareMessage mutatedMessage = message.withSequenceNumber(message.getSequenceNumber() - random.nextLong(bound));
+                //         PrepareMessage mutatedMessage = message.withSequenceNumber(message.getSequenceNumber() - 1);
+                //         mutatedMessage.sign(message.getSignedBy());
+                //         messageEvent.setPayload(mutatedMessage);
+                //     }
+                // },
+                // new MessageMutationFault("hbft-prepare-different-digest", "Change digest", List.of(PrepareMessage.class)) {
+                //     @Override
+                //     public void accept(FaultContext serializable) {
+                //         Optional<Event> event = serializable.getEvent();
+                //         if (event.isEmpty()) {
+                //             throw invalidMessageTypeException;
+                //         }
+                //         if (!(event.get() instanceof MessageEvent messageEvent)) {
+                //             throw invalidMessageTypeException;
+                //         }
+                //         if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
+                //             throw invalidMessageTypeException;
+                //         }
+                //         // Create a random digest
+                //         byte[] digest = new byte[20];
+                //         PrepareMessage mutatedMessage = message.withDigest(digest);
                 //         mutatedMessage.sign(message.getSignedBy());
                 //         messageEvent.setPayload(mutatedMessage);
                 //     }
                 // }
+
+                // ANY-SCOPE mutations
+                //,
+                new MessageMutationFault("hbft-prepare-view-inc", "Increment View Number", List.of(PrepareMessage.class)) {
+                    @Override
+                    public void accept(FaultContext serializable) {
+                        Optional<Event> event = serializable.getEvent();
+                        if (event.isEmpty()) {
+                            throw invalidMessageTypeException;
+                        }
+                        if (!(event.get() instanceof MessageEvent messageEvent)) {
+                            throw invalidMessageTypeException;
+                        }
+                        if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
+                            throw invalidMessageTypeException;
+                        }
+                        PrepareMessage mutatedMessage = message.withViewNumber(message.getViewNumber() + random.nextLong(bound));
+                        mutatedMessage.sign(message.getSignedBy());
+                        messageEvent.setPayload(mutatedMessage);
+                    }
+                },
+                new MessageMutationFault("hbft-prepare-view-dec", "Decrement View Number", List.of(PrepareMessage.class)) {
+                    @Override
+                    public void accept(FaultContext serializable) {
+                        Optional<Event> event = serializable.getEvent();
+                        if (event.isEmpty()) {
+                            throw invalidMessageTypeException;
+                        }
+                        if (!(event.get() instanceof MessageEvent messageEvent)) {
+                            throw invalidMessageTypeException;
+                        }
+                        if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
+                            throw invalidMessageTypeException;
+                        }
+                        PrepareMessage mutatedMessage = message.withViewNumber(message.getViewNumber() - random.nextLong(bound));
+                        mutatedMessage.sign(message.getSignedBy());
+                        messageEvent.setPayload(mutatedMessage);
+                    }
+                },
+                new MessageMutationFault("hbft-prepare-seq-inc", "Increment Sequence Number", List.of(PrepareMessage.class)) {
+                    @Override
+                    public void accept(FaultContext serializable) {
+                        Optional<Event> event = serializable.getEvent();
+                        if (event.isEmpty()) {
+                            throw invalidMessageTypeException;
+                        }
+                        if (!(event.get() instanceof MessageEvent messageEvent)) {
+                            throw invalidMessageTypeException;
+                        }
+                        if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
+                            throw invalidMessageTypeException;
+                        }
+
+                        PrepareMessage mutatedMessage = message.withSequenceNumber(message.getSequenceNumber() + random.nextLong(bound));
+                        mutatedMessage.sign(message.getSignedBy());
+                        messageEvent.setPayload(mutatedMessage);
+                    }
+                },
+                new MessageMutationFault("hbft-prepare-sec-dec", "Decrement Sequence Number", List.of(PrepareMessage.class)) {
+                    @Override
+                    public void accept(FaultContext serializable) {
+                        Optional<Event> event = serializable.getEvent();
+                        if (event.isEmpty()) {
+                            throw invalidMessageTypeException;
+                        }
+                        if (!(event.get() instanceof MessageEvent messageEvent)) {
+                            throw invalidMessageTypeException;
+                        }
+                        if (!(messageEvent.getPayload() instanceof PrepareMessage message)) {
+                            throw invalidMessageTypeException;
+                        }
+                        PrepareMessage mutatedMessage = message.withSequenceNumber(message.getSequenceNumber() - random.nextLong(bound));
+                        mutatedMessage.sign(message.getSignedBy());
+                        messageEvent.setPayload(mutatedMessage);
+                    }
+                }
         
         );
     }
