b9f12ee82187a9c1df1817c199d082b04b6ca083
João Neto <1539767+joaomlneto@users.noreply.github.com>
2025-04-08 17:41:18 +0200

    Campaign: get schedule IDs when retrieving as JSON

--- simulator/src/main/java/byzzbench/simulator/domain/Campaign.java
+++ simulator/src/main/java/byzzbench/simulator/domain/Campaign.java
@@ -1,5 +1,6 @@
 package byzzbench.simulator.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -34,5 +35,15 @@ public class Campaign {
      * The schedules saved for this campaign.
      */
     @OneToMany(mappedBy = "campaign", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnore
     private List<Schedule> schedules = new ArrayList<>();
+
+    /**
+     * Get the list of schedule IDs for this campaign.
+     *
+     * @return a list of schedule IDs
+     */
+    public List<Long> getScheduleIds() {
+        return schedules.stream().map(Schedule::getScheduleId).toList();
+    }
 }
