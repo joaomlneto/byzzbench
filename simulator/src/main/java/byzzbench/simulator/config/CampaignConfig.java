19ff7861ae4c26eed641a151de1f9af04492f66a
João Neto <1539767+joaomlneto@users.noreply.github.com>
2025-10-24 14:59:01 +0200

    CampaignConfig: document fields

--- simulator/src/main/java/byzzbench/simulator/config/CampaignConfig.java
+++ simulator/src/main/java/byzzbench/simulator/config/CampaignConfig.java
@@ -1,17 +1,44 @@
 package byzzbench.simulator.config;
 
+import byzzbench.simulator.domain.Campaign;
 import byzzbench.simulator.domain.ScenarioParameters;
 import byzzbench.simulator.exploration_strategy.ExplorationStrategyParameters;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * Represents the configuration for setting up a {@link Campaign}
+ */
 @Data
 @Builder
 public class CampaignConfig {
+    /**
+     * The initial random seed, from which to seed each scenario
+     */
     private long initialRandomSeed;
+    
+    /**
+     * The ID of the exploration strategy to be used
+     */
     private String explorationStrategyId;
-    private long numScenarios;
-    private ScenarioParameters scenarioParameters;
+
+    /**
+     * The parameters for the exploration strategy
+     */
     private ExplorationStrategyParameters explorationStrategyParameters;
+
+    /**
+     * The number of scenarios to run for this campaign
+     */
+    private long numScenarios;
+
+    /**
+     * The parameters to initialize the scenario
+     */
+    private ScenarioParameters scenarioParameters;
+
+    /**
+     * Parameters for terminating the execution of a scenario
+     */
     private TerminationConfig termination;
 }
