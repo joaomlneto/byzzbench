928e1e3e0566fa2416d8bdab09729738738aa06b
João Neto <1539767+joaomlneto@users.noreply.github.com>
2025-10-31 15:39:16 +0100

    Update ByzzFuzzScenarioStrategyData.java

--- simulator/src/main/java/byzzbench/simulator/exploration_strategy/byzzfuzz/ByzzFuzzScenarioStrategyData.java
+++ simulator/src/main/java/byzzbench/simulator/exploration_strategy/byzzfuzz/ByzzFuzzScenarioStrategyData.java
@@ -11,10 +11,23 @@ import java.util.Map;
 @Getter
 @SuperBuilder
 public class ByzzFuzzScenarioStrategyData extends ScenarioStrategyData {
-    private final int numRoundsWithProcessFaults;
-    private final int numRoundsWithNetworkFaults;
-    private final int numRoundsWithFaults;
+    /**
+     * The faults to be injected during the execution of the scenario
+     */
     private final List<Fault> faults;
+
+    /**
+     * Information about replica progress
+     */
     private final Map<String, ByzzFuzzRoundInfo> roundInfos;
+
+    /**
+     * The estimated round in which the replica is in
+     */
     private final Map<String, Long> replicaRounds;
+
+    /**
+     * Maps the ID of a message to the round the replica was in
+     */
+    private final Map<Long, Long> messageRound;
 }
