70fe2dfd2842c0700ebe9950f01424490e571531
João Neto <1539767+joaomlneto@users.noreply.github.com>
2025-12-02 15:33:31 +0100

    fix OutputLogger: add missing methods

--- simulator/src/main/java/byzzbench/simulator/OutputLogger.java
+++ simulator/src/main/java/byzzbench/simulator/OutputLogger.java
@@ -1,10 +1,8 @@
 package byzzbench.simulator;
 
 import byzzbench.simulator.faults.Fault;
-import byzzbench.simulator.transport.Event;
-import byzzbench.simulator.transport.MutateMessageEventPayload;
-import byzzbench.simulator.transport.TimeoutEvent;
-import byzzbench.simulator.transport.TransportObserver;
+import byzzbench.simulator.nodes.Node;
+import byzzbench.simulator.transport.*;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -12,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.Instant;
+import java.util.SortedSet;
 
 @Slf4j
 public class OutputLogger implements TransportObserver {
@@ -27,6 +26,10 @@ public class OutputLogger implements TransportObserver {
         } catch (IOException e) {
             log.error("Can't create output directory: {}", file.toAbsolutePath(), e);
         }
+    }
+
+    @Override
+    public void onMulticast(Node sender, SortedSet<String> recipients, MessagePayload payload) {
     }
 
     @Override
@@ -60,6 +63,11 @@ public class OutputLogger implements TransportObserver {
 
     @Override
     public void onTimeout(TimeoutEvent event) {
+
+    }
+
+    @java.lang.Override
+    public void onGlobalStabilizationTime() {
 
     }
 
